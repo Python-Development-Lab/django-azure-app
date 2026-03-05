fc2d1a635d86b8b7c47b02213c687e5d7caddc7b
Vitalii Shevchuk <126573180+VitaliiShevchuk2023@users.noreply.github.com>
2026-03-05 09:54:32 +0000

    fix: show login.html page before Microsoft redirect

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='auth_login'),
+    path('redirect/', views.microsoft_redirect_view, name='auth_redirect'),
     path('callback/', views.callback_view, name='auth_callback'),
     path('logout/', views.logout_view, name='auth_logout'),
 ]
