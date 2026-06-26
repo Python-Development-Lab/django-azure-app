0b17266e0f4860453757bd369fca125036bbd1aa
Vitalii Shevchuk <126573180+VitaliiShevchuk2023@users.noreply.github.com>
2026-06-26 17:28:19 +0000

    fix: explicitly set auth_app loggers level for OpenTelemetry capture

--- djangoapp/wsgi.py
+++ djangoapp/wsgi.py
@@ -20,6 +20,12 @@ try:
     if _conn:
         from azure.monitor.opentelemetry import configure_azure_monitor
         configure_azure_monitor(connection_string=_conn)
+        # Підключити auth_app logger до OpenTelemetry
+        import logging
+        from opentelemetry.sdk._logs import LoggerProvider
+        from opentelemetry._logs import get_logger_provider
+        for log_name in ["auth_app", "auth_app.device_middleware", "djangoapp"]:
+            logging.getLogger(log_name).setLevel(logging.INFO)
         logger.warning("Azure Monitor: OpenTelemetry configured")
     else:
         logger.warning("Azure Monitor: APPINSIGHTS_CONNECTION_STRING not set")
